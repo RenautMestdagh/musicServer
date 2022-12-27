84875faf1be79779a60d6322019b9648159002c7
RenautMestdagh <renaut.mestdagh@hotmail.com>
2022-12-27 08:18:02 +0100

    less output from ffmpeg

--- routes/index.js
+++ routes/index.js
@@ -265,7 +265,7 @@ async function getLinks() {
                     try {
                         const {data} = await axios.get(Object.values(response.requested_formats)[1].url, {responseType: 'arraybuffer'});
                         fs.writeFileSync(path.join(__dirname, '../tmp/songs/'+ytId+".webm"), data, 'binary');
-                        execSync('ffmpeg -i '+path.join(__dirname, '../tmp/songs/'+ytId+".webm")+' -vn '+path.join(__dirname, '../tmp/songs/'+ytId+".mp3"), { encoding: 'utf-8' });  // the default is 'buffer'
+                        execSync('ffmpeg -hide_banner -loglevel error -i '+path.join(__dirname, '../tmp/songs/'+ytId+".webm")+' -vn '+path.join(__dirname, '../tmp/songs/'+ytId+".mp3"), { encoding: 'utf-8' });  // the default is 'buffer'
                         fs.unlinkSync(path.join(__dirname, '../tmp/songs/'+ytId+".webm"))
                         currentAtSameTime--
                         songDone(ytId)
@@ -340,7 +340,7 @@ async function songDone(ytId) {
     }
 
     await ffmetadata.write(path.join(__dirname, '../tmp/songs/'+ytId+".mp3"), tags,  function(err) {
-        execSync('ffmpeg -i '+path.join(__dirname, '../tmp/songs/'+ytId+".mp3")+' -i '+path.join(__dirname, '../tmp/img/'+ytId+".jpg -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title=\"Album cover\" -metadata:s:v comment=\"Cover (front)\" "+libPath+ytId+".mp3"), { encoding: 'utf-8' });  // the default is 'buffer'
+        execSync('ffmpeg -hide_banner -loglevel error -i '+path.join(__dirname, '../tmp/songs/'+ytId+".mp3")+' -i '+path.join(__dirname, '../tmp/img/'+ytId+".jpg -map 0:0 -map 1:0 -c copy -id3v2_version 3 -metadata:s:v title=\"Album cover\" -metadata:s:v comment=\"Cover (front)\" "+libPath+ytId+".mp3"), { encoding: 'utf-8' });  // the default is 'buffer'
         fs.unlinkSync( path.join(__dirname, '../tmp/songs/'+ytId+".mp3"));
         fs.unlinkSync(path.join(__dirname, '../tmp/img/' + ytId + ".jpg"));
     })
